be2cdaf40aa5f3a604b328eb31c129dbeffadc20
wyguy444 <wyattlpaul@gmail.com>
2023-08-04 09:58:40 -0700

    todo-list homework

---
+++ todo-list/src/App.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react'
+
+function App() {
+
+    const [list, setList] = useState([]);
+    const [input, setInput] = useState("");
+
+    const addTodo = (todo) => {
+        const newTodo = {
+            id: Math.random(),
+            todo: todo
+        };
+
+        setList([...list, newTodo]);
+
+        setInput("");
+    }
+
+    const deleteTodo = (id) => {
+        const newList = list.filter((todo) => todo.id !== id);
+        setList(newList);
+    }
+
+    return (
+        <div>
+            <h1>Todo List</h1>
+            <input type="text" value={input} onChange={(e) => setInput(e.target.value)}/>
+            <button onClick={() => addTodo(input)}>Add</button>
+            <ul>
+                {list.map((todo) => (
+                    <li key={todo.id}>
+                        {todo.todo}
+                        <button onClick={() => deleteTodo(todo.id)}>&times;</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default App
